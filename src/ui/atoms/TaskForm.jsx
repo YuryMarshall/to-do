f6ffef48dcac28d58a4ac428dd1962657a7dbe6f
YuryMarshall <oomichbird@gmail.com>
2023-07-06 11:00:01 +0300

    changed text of sybmol counter

--- src/ui/atoms/TaskForm.jsx
+++ src/ui/atoms/TaskForm.jsx
@@ -18,8 +18,8 @@ export default function TaskForm(props) {
         Добавить
       </button>
       <p className={props.value ? styles.symbols__number : "hidden"}>
-        Осталось {29 - props.value.length >= 0 ? 29 - props.value.length : "0"}{" "}
-        символов
+        Осталось символов:{" "}
+        {29 - props.value.length >= 0 ? 29 - props.value.length : "0"}
       </p>
     </form>
   );
